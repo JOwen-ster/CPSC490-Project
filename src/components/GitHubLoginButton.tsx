b55391dbb7a2220426712a0adacf1b13d70c81b5
JOwen-ster <jowen.sterling@csu.fullerton.edu>
2025-09-17 23:50:27 -0700

    Update github login button

--- src/components/GitHubLoginButton.tsx
+++ src/components/GitHubLoginButton.tsx
@@ -2,11 +2,11 @@ export default async function GitHubLoginButton() {
     return (
         <div className="flex justify-center">
             <a
-                href="example.com"
-                className="group flex items-center gap-2 rounded-xl border border-black bg-black px-4 py-2 text-sm font-medium text-white shadow-sm transition-colors duration-200 hover:bg-white hover:text-black"
+                href="https://www.github.com"
+                className="group flex items-center gap-2 rounded-xl border border-black bg-black px-4 py-2 text-sm font-medium text-white shadow-sm transition-all duration-500 ease-in-out hover:bg-white hover:text-black"
             >
                 <svg
-                    className="h-5 w-5 fill-current transition-colors duration-200 group-hover:fill-black"
+                    className="h-5 w-5 fill-current transition-all duration-500 ease-in-out group-hover:fill-black"
                     viewBox="0 0 98 96"
                     xmlns="http://www.w3.org/2000/svg"
                 >
